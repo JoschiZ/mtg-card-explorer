68ea24ad3ec7dbdbefe7c389e98f384c5585fd99
joschi <joschi.grey@posteo.de>
2026-06-06 16:42:22 +0200

    Fix exploration size

---
+++ src/CardExplorer/Migrations/20260606144100_Increase Exploration Search string lenght.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CardExplorer.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncreaseExplorationSearchstringlenght : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SearchString",
+                table: "Exploration",
+                type: "character varying(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SearchString",
+                table: "Exploration",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(1000)",
+                oldMaxLength: 1000);
+        }
+    }
+}
